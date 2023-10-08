086a80afaaf110a704034a27548280d8e2002ac0
victor-schmitz <victorcavalcan7i@gmail.com>
2023-10-08 11:43:44 -0300

    DESAFIO 08

---
+++ DESAFIO 08/DESAFIO 08/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace Desafio08;
+
+class Program
+{
+    static void Main()
+    {
+        // Var
+        float prova1, prova2, prova3, provaFinal, media, mediaFinal;
+
+        Console.WriteLine("Bem vindo a Calculadora de Médias da ADA!");
+
+        do
+        {
+            Console.Write("\nDigite a nota da sua primeira prova: ");
+        } while (!float.TryParse(Console.ReadLine(), out prova1) || prova1 < 0 || prova1 > 10);
+
+        do
+        {
+            Console.Write("\nDigite a nota da sua segunda prova: ");
+        } while (!float.TryParse(Console.ReadLine(), out prova2) || prova2 < 0 || prova2 > 10);
+
+        do
+        {
+            Console.Write("\nDigite a nota da sua terceira prova: ");
+        } while (!float.TryParse(Console.ReadLine(), out prova3) || prova3 < 0 || prova3 > 10);
+
+        media = (prova1 + prova2 + prova3) / 3;
+
+        if (media >= 7)
+        {
+            Console.WriteLine($"\nParabéns, você foi aprovado!\nSua média foi {media:F2}");
+        }
+        else
+        {
+            Console.WriteLine($"\nVocê ainda não foi aprovado.\nSua média foi {media:F2}");
+            do
+            {
+                Console.Write("\nDigite a nota da sua prova final: ");
+            } while (!float.TryParse(Console.ReadLine(), out provaFinal) || provaFinal < 0 || provaFinal > 10);
+
+            mediaFinal = (provaFinal + media) / 2;
+
+            if (mediaFinal >= 5)
+            {
+                Console.WriteLine($"\nParabéns, você foi aprovado na final!\nSua média foi {mediaFinal:F2}");
+            }
+            else
+            {
+                Console.WriteLine($"\nVocê não foi aprovado.\nSua média foi {mediaFinal:F2}");
+            }
+        }
+    }
+}
---
+++ DESAFIO 08/README.md
@@ -0,0 +1,9 @@
+## DESAFIO 05: Verificador de Peso e Multa de Peixes
+Sr. José, pescador, comprou um microcomputador para
+controlar o rendimento diário de seu trabalho. Toda vez que ele traz um peso de
+peixes maior que o estabelecido pelo regulamento de pesca do estado de São Paulo
+(50 quilos) deve pagar uma multa de R$4,00 por quilo excedente. Sr. José, precisa que
+você faça programa que leia o peso de peixes e verifique se há excesso.
+Se houver, gravar o Excesso e o valor da multa que Sr. José
+deverá pagar. Caso contrário mostrar os valores com o conteúdo ZERO. 
+Apresentar as mensagens no console.
