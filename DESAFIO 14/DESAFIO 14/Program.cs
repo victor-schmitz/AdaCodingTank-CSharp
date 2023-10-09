a58f9617a06068176e17bec9a1a9d678c079fcc0
victor-schmitz <victorcavalcan7i@gmail.com>
2023-10-08 22:45:46 -0300

    DESAFIO 14

---
+++ DESAFIO 14/DESAFIO 14/Program.cs
@@ -0,0 +1,23 @@
+﻿//EXERCÍCIO 03
+
+//Some os números de 1 a 100 a imprima o valor.
+
+namespace Desafio14;
+
+class Program
+{
+    static void Main()
+    {
+        int soma = 0;
+
+        Console.WriteLine("Bem vindo ao somatório de 1 a 100 da ADA!");
+
+        for (int i = 1; i <= 100; i++)
+        {
+            Console.Write($"[{i}] ");
+            soma += i;
+        }
+
+        Console.WriteLine($"\n\nA soma de todos os elementos entre 1 e 100 é igual a {soma}");
+    }
+}
---
+++ DESAFIO 14/README.md
@@ -0,0 +1,3 @@
+## DESAFIO 10: Média, Soma e o Menor Valor
+Crie um programa que peça 10 números inteiros
+e apresente a média, a soma e o menor.
