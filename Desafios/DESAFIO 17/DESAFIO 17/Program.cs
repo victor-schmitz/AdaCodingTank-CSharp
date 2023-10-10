11f6b09c4e9199a008191bc173f70116eab6b991
victor-schmitz <victorcavalcan7i@gmail.com>
2023-10-10 14:52:29 -0300

    DESAFIO 17

---
+++ Desafios/DESAFIO 17/DESAFIO 17/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace Desafio17;
+
+class Program
+{
+    static void Main()
+    {
+        int termoParada = 0;
+        int[] fibonacci = new int[1];
+        try
+        {
+            while (true)
+            {
+                Console.Write("Digite o número de parada da sequencia: ");
+                if (!int.TryParse(Console.ReadLine(), out termoParada))
+                {
+                    Console.WriteLine("Digite um número inteiro.");
+                }
+                else if (termoParada < 0)
+                {
+                    Console.WriteLine("Digite um número maior que 0");
+                }
+                else
+                {
+                    break;
+                }
+            }
+            Array.Resize(ref fibonacci, termoParada);
+
+            if (termoParada >= 1)
+            {
+                fibonacci[0] = 0;
+            }
+            if (termoParada >= 2)
+            {
+                fibonacci[1] = 1;
+            }
+
+            for (int i = 2; i < termoParada; i++)
+            {
+                fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+            }
+
+            Console.Write($"Os {termoParada} primeiros números da sequencia fibonacci são: ");
+
+            for (int i = 0; i < fibonacci.Length; i++)
+            {
+                Console.Write($"[{fibonacci[i]}] ");
+            }
+        } catch (Exception ex)
+        {
+            Console.WriteLine("Digite valores que sejam válidos na próxima vez que rodar");
+        }
+    }
+}
